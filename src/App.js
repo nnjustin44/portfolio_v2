d1fe61e06de7f7911c204a9bf1bf991426d64074
nnjustin44 <nnjustin44@gmail.com>
2021-09-25 22:17:14 -0400

    formatting header

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import  React  from 'react';
 import './App.css';
-import {profile_pic} from './AppConstants.js';
-import {skillSet1} from './AppConstants.js';
-import {skillSet2} from './AppConstants.js';
-import {skillSet3} from './AppConstants.js';
-import {skillSet4} from './AppConstants.js';
+import {background_pic, profile_pic,skillSet1,skillSet2,skillSet3,skillSet4} from './AppConstants.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav className="navbar" role="navigation">
-
-        </nav>
-        <div>
+        <div className="topHeader">
+          
+        <div className="nav">
+          hello
+        </div>
+          <img src= {background_pic} className="background_pic"></img>
           <img src={profile_pic} className="profile_pic"></img>
         </div>
-      </header>
-        <body>
+
+          
+          <div className="title">
+            SkillSet
+          </div>
+          <div className="body">
+
           <div>   
             <img className="skillSet1" src={skillSet1}></img>
           </div>
           <div>
             <img className="skillSet2" src={skillSet2}></img>
-           </div>
-           <div>
+          </div>
+          <div>
             <img className="skillSet3" src={skillSet3}></img>
           </div>
           <div>          
             <img className="skillSet4" src={skillSet4}></img>
           </div>
-        </body>
+        </div>
     </div>
 
   );
--- src/AppConstants.js
+++ src/AppConstants.js
@@ -5,6 +5,7 @@
 
 
 export const profile_pic = "https://cdn.discordapp.com/attachments/519146706118508544/867934138617978890/profile_pic.png";
+export const background_pic = "https://cdn.discordapp.com/attachments/881162947655786496/890432932823572511/background_discord.jpg";
 
 /*******Skill Sets*******/
 export const skillSet1 = "https://cdn.discordapp.com/attachments/816358803163447387/873958459538833408/logo192.png";
