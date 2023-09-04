3af151f190ff7d977ec9efdc546dcaf5fdd7c30b
Justin Nguyen <45019950+nnjustin44@users.noreply.github.com>
2023-09-04 12:27:56 -0400

    update background, resume, and some styling

--- src/App.js
+++ src/App.js
@@ -1,95 +1,93 @@
-import  React  from 'react';
+import React from "react";
 import { dateTime } from "./dateTime.js";
-import Button from '@mui/material/Button';
-import './App.css';
-import {background_pic, profile_pic, skillSet1, skillSet2, skillSet3, skillSet4, resumeLink, githubLink} from './AppConstants.js';
-
+import Button from "@mui/material/Button";
+import "./App.css";
+import {
+  background_pic,
+  profile_pic,
+  skillSet1,
+  skillSet2,
+  skillSet3,
+  skillSet4,
+  resumeLink,
+  githubLink,
+} from "./AppConstants.js";
 
 function App() {
   return (
     <div className="App">
       <title>Justin Nguyen Portfolio</title>
       <div className="topHeader">
-        <div className="nav">
-        </div>
-        <img src= {background_pic} className="background_pic"></img>
+        <div className="nav"></div>
+        <img src={background_pic} className="background_pic"></img>
         <img src={profile_pic} className="profile_pic"></img>
-          <div className="titleTop">
-            Justin Nguyen
-          </div>
+        <div className="titleTop">Justin Nguyen</div>
       </div>
-          <div className="menu">
-            <a href= "#homeSection" className="skillsetNavbar">
-              <Button className="btn">Skillset</Button>
-            </a> 
-            <a href= "#bioSection" className="aboutNavbar">
-              <Button className="btn">About</Button>
-            </a>
-            <a href= "#connectSection" className="connectNavbar">
-              <Button className="btn">Connect</Button>
-            </a>
-            <a
-                className = "resumeNavBar"
-                href= {resumeLink}
-                >
-              <Button className="btn">Resume</Button>
-            </a>
-            <a 
-               className="githubNavbar" 
-               href={githubLink}
-              >
-              <Button className="btn">Github</Button>
-            </a>
-          {/* <p className="dateTime">{dateTime()}</p>  */}
+      <div className="menu">
+        <a href="#homeSection" className="skillsetNavbar">
+          <Button className="btn">Skillset</Button>
+        </a>
+        <a href="#bioSection" className="aboutNavbar">
+          <Button className="btn">About</Button>
+        </a>
+        <a href="#connectSection" className="connectNavbar">
+          <Button className="btn">Connect</Button>
+        </a>
+        <a className="resumeNavBar" href={resumeLink}>
+          <Button className="btn">Resume</Button>
+        </a>
+        <a className="githubNavbar" href={githubLink}>
+          <Button className="btn">Github</Button>
+        </a>
+        {/* <p className="dateTime">{dateTime()}</p>  */}
+      </div>
+
+      <div className="body">
+        <section id="homeSection">
+          <div className="skillsetWrapper">
+            <h1 className="skillTitle">Skillset</h1>
+            <div>
+              <img className="skillSet1" src={skillSet1}></img>
+            </div>
+            <div>
+              <img className="skillSet2" src={skillSet2}></img>
+            </div>
+            <div>
+              <img className="skillSet3" src={skillSet3}></img>
+            </div>
+            <div>
+              <img className="skillSet4" src={skillSet4}></img>
+            </div>
           </div>
+        </section>
 
-          <div className="body"> 
-            <section id="homeSection">
-              <div className="skillsetWrapper">
-                <h1 className="skillTitle">
-                  Skillset
-                </h1>
-                <div>   
-                  <img className="skillSet1" src={skillSet1}></img>
-                </div>
-                <div>
-                  <img className="skillSet2" src={skillSet2}></img>
-                </div>
-                <div>
-                  <img className="skillSet3" src={skillSet3}></img>
-                </div>
-                <div>          
-                  <img className="skillSet4" src={skillSet4}></img>
-                </div>
-              </div>
-            </section>
-            
-            <section id="bioSection">
-              <div className="bio"> 
-                <h1 className="title">
-                About
-                </h1>
-                <p>Hi! I'm Justin Nguyen, I am a self taught programmer.</p>
-                <p>In the pursuit of excellence, driven for self growth in software development.</p>
-                <p>This Portfolio was programmed from scratch using React.js and hosted on Google Firebase.</p>
-                <p>I have been a developer at Fiserv since 2021.</p>
-                <p>Honest. Creative. Dedicated.</p>
-              </div>
-            </section>
+        <section id="bioSection">
+          <div className="bio">
+            <h1 className="title">About</h1>
+            <p>Hi! I'm Justin Nguyen, I am a self taught programmer.</p>
+            <p>
+              In the pursuit of excellence, driven for self growth in software
+              development.
+            </p>
+            <p>
+              This Portfolio was programmed from scratch using React.js and
+              hosted on Google Firebase.
+            </p>
+            <p>I have been a developer at Fiserv since 2021.</p>
+            <p>Honest. Creative. Dedicated.</p>
+          </div>
+        </section>
 
-            <section id="connectSection">
-              <div className="connect"> 
-                <h1 className="title">
-                Connect
-                </h1>
-                <p>Email: nnjustin44@gmail.com </p>
-                <p>·       ·       ·</p>
-                <p>404-789-0044</p>
-              </div>
-            </section>
-        </div>
+        <section id="connectSection">
+          <div className="connect">
+            <h1 className="title">Connect</h1>
+            <p>Email: nnjustin44@gmail.com</p>
+            <p>· · ·</p>
+            <p>Phone: 404-789-0044</p>
+          </div>
+        </section>
+      </div>
     </div>
-
   );
 }
 
--- src/AppConstants.js
+++ src/AppConstants.js
@@ -1,17 +1,18 @@
-
-
-
-    
-
-
-export const profile_pic = "https://cdn.discordapp.com/attachments/816358803163447387/891777884975403018/Screen_Shot_2021-08-26_at_10.12.09.png";
-export const background_pic = "https://cdn.discordapp.com/attachments/881162947655786496/1147260420881649714/Screenshot_2023-09-01_at_16.03.12.png";
+export const profile_pic =
+  "https://cdn.discordapp.com/attachments/816358803163447387/891777884975403018/Screen_Shot_2021-08-26_at_10.12.09.png";
+export const background_pic =
+  "https://cdn.discordapp.com/attachments/881162947655786496/1147260420881649714/Screenshot_2023-09-01_at_16.03.12.png";
 /*******Skill Sets*******/
-export const skillSet1 = "https://cdn.discordapp.com/attachments/816358803163447387/873958459538833408/logo192.png";
-export const skillSet2 = "https://cdn.discordapp.com/attachments/578355339133452316/888537189946908742/csharp-logo.png";
-export const skillSet3 = "https://cdn.discordapp.com/attachments/578355339133452316/888537229113307196/javascript-logo.png";
-export const skillSet4 = "https://cdn.discordapp.com/attachments/811991353969213502/888541955414429696/2048px-Python-logo-notext.svg.png";
+export const skillSet1 =
+  "https://cdn.discordapp.com/attachments/816358803163447387/873958459538833408/logo192.png";
+export const skillSet2 =
+  "https://cdn.discordapp.com/attachments/578355339133452316/888537189946908742/csharp-logo.png";
+export const skillSet3 =
+  "https://cdn.discordapp.com/attachments/578355339133452316/888537229113307196/javascript-logo.png";
+export const skillSet4 =
+  "https://cdn.discordapp.com/attachments/811991353969213502/888541955414429696/2048px-Python-logo-notext.svg.png";
 
 /*****Resume Link******/
-export const resumeLink = "https://cdn.discordapp.com/attachments/787777223855439893/1072599187369234494/Screenshot_2023-02-07_at_14.25.50.png";
+export const resumeLink =
+  "https://cdn.discordapp.com/attachments/816358803163447387/1148292235952144394/Screenshot_2023-09-04_at_12.23.16.png";
 export const githubLink = "https://github.com/nnjustin44/portfolio_v2";
