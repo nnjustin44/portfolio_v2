c98fd2b88d976ccc6d7d7f8d00aca1e9b5563f2b
Justin Nguyen <45019950+nnjustin44@users.noreply.github.com>
2023-09-05 23:44:54 -0400

    change github link and some padding

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -15,7 +15,7 @@ export const skillSet4 =
 /*****Resume Link******/
 export const resumeLink =
   "https://cdn.discordapp.com/attachments/816358803163447387/1148346424778567720/Screenshot_2023-09-04_at_15.58.36.png";
-export const githubLink = "https://github.com/nnjustin44/portfolio_v2";
+export const githubLink = "https://github.com/nnjustin44";
 
 /*****LinkedIn Link******/
 export const linkedInLink =
