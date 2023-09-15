c356753cd8f16b499de55f5a7deda6d15455fa78
Justin Nguyen <45019950+nnjustin44@users.noreply.github.com>
2023-09-15 16:45:24 -0400

    title padding fixed

--- src/App.js
+++ src/App.js
@@ -30,45 +30,53 @@ function App() {
         <img src={background_pic} className="background_pic" />
         <div className="titleTop">Justin Nguyen</div>
       </div>
-      <div className="menu">
-        <a href="#homeSection" className="menuNavbar">
-          <Button className="nav-btn">Home</Button>
-        </a>
-        <a href="#skillSection" className="menuNavbar">
-          <Button className="nav-btn">Skillset</Button>
-        </a>
-        <a href="#bioSection" className="menuNavbar">
-          <Button className="nav-btn">About</Button>
-        </a>
-        <a href="#experienceSection" className="menuNavbar">
-          <Button className="nav-btn">Experience</Button>
-        </a>
-        <a href="#connectSection" className="menuNavbar">
-          <Button className="nav-btn">Connect</Button>
-        </a>
-        <a className="menuNavbar">
-          <Button className="nav-btn" onClick={() => openInNewTab(githubLink)}>
-            Github
-          </Button>
-        </a>
-        <a className="menuNavbar">
-          <Button className="nav-btn" onClick={() => openInNewTab(resumeLink)}>
-            Resume
-          </Button>
-        </a>
-      </div>
-      <div className="body">
-        <div id="skillSection">
-          <SkillSection id="skillSection" />
+      <div className="main-body">
+        <div className="menu">
+          <a href="#homeSection" className="menuNavbar">
+            <Button className="nav-btn">Home</Button>
+          </a>
+          <a href="#skillSection" className="menuNavbar">
+            <Button className="nav-btn">Skillset</Button>
+          </a>
+          <a href="#bioSection" className="menuNavbar">
+            <Button className="nav-btn">About</Button>
+          </a>
+          <a href="#experienceSection" className="menuNavbar">
+            <Button className="nav-btn">Experience</Button>
+          </a>
+          <a href="#connectSection" className="menuNavbar">
+            <Button className="nav-btn">Connect</Button>
+          </a>
+          <a className="menuNavbar">
+            <Button
+              className="nav-btn"
+              onClick={() => openInNewTab(githubLink)}
+            >
+              Github
+            </Button>
+          </a>
+          <a className="menuNavbar">
+            <Button
+              className="nav-btn"
+              onClick={() => openInNewTab(resumeLink)}
+            >
+              Resume
+            </Button>
+          </a>
         </div>
-        <div id="bioSection">
-          <BioSection />
-        </div>
-        <div id="experienceSection">
-          <ExperienceSection />
-        </div>
-        <div id="connectSection">
-          <ConnectSection />
+        <div className="info-body">
+          <div id="skillSection">
+            <SkillSection id="skillSection" />
+          </div>
+          <div id="bioSection">
+            <BioSection />
+          </div>
+          <div id="experienceSection">
+            <ExperienceSection />
+          </div>
+          <div id="connectSection">
+            <ConnectSection />
+          </div>
         </div>
       </div>
     </div>
