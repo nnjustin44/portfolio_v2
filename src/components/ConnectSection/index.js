0e5fc2f1912d8d7bc92486d51eab8c1e271c5c75
Justin Nguyen <45019950+nnjustin44@users.noreply.github.com>
2024-03-21 00:55:15 -0400

    email link button

--- src/components/ConnectSection/index.js
+++ src/components/ConnectSection/index.js
@@ -4,23 +4,32 @@ import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import openInNewTab from "../helpers/openNewTab";
 import { LINKS } from "../../AppConstants";
+import "./styles.scss";
 
 const ConnectSection = ({ deviceSize }) => {
+  const sendEmail = () => {
+    window.location =
+      "mailto:nnjustin44@gmail.com?subject=Saying Hello!&body=Hello!";
+  };
   return (
     <div className="connect">
       <h1 className={deviceSize}>Connect</h1>
-      <div className="connectIcons">
+      <div className="connectButton">
         <MailOutlineIcon />:
-        <div className="sub-item"> nnjustin44@gmail.com</div>
+        <a>
+          <button className="connect-btn" onClick={sendEmail}>
+            nnjustin44@gmail.com
+          </button>
+        </a>
       </div>
       <div className="connectIcons">
         <PhoneIphoneIcon />:<div className="sub-item">404-789-0044</div>
       </div>
-      <div className="linkedIn">
+      <div className="connectButton">
         <LinkedInIcon />:
         <a>
           <button
-            className="linkedIn-btn"
+            className="connect-btn"
             onClick={() => openInNewTab(LINKS.LINKEDIN_LINK)}
           >
             LinkedIn
--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -3,31 +3,55 @@ import MobileMenu from "./MobileMenu";
 import { Button } from "@mui/material";
 import openInNewTab from "../helpers/openNewTab";
 import { LINKS } from "../../AppConstants";
+import HomeIcon from "@mui/icons-material/Home";
+import HandymanIcon from "@mui/icons-material/Handyman";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import EngineeringIcon from "@mui/icons-material/Engineering";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import DescriptionIcon from "@mui/icons-material/Description";
 
 const MenuHeader = ({ width, height }) => {
   const Menu = () => {
     return (
       <div className="menu">
         <a href="#homeSection" className="menuNavbar">
-          <Button className="nav-btn">Home</Button>
+          <Button className="nav-btn">
+            <HomeIcon className="menuIcon" color="primary" />
+            Home
+          </Button>
         </a>
         <a href="#jonahSection" className="menuNavbar">
-          <Button className="nav-btn">J.O.N.A.H</Button>
+          <Button className="nav-btn">
+            <AccessibilityNewIcon className="menuIcon" color="primary" />
+            J.O.N.A.H
+          </Button>
         </a>
         <a href="#skillSection" className="menuNavbar">
-          <Button className="nav-btn">Skillset</Button>
+          <Button className="nav-btn">
+            <HandymanIcon className="menuIcon" color="primary" />
+            Skillset
+          </Button>
         </a>
         <a href="#experienceSection" className="menuNavbar">
-          <Button className="nav-btn">Experience</Button>
+          <Button className="nav-btn">
+            <EngineeringIcon className="menuIcon" color="primary" />
+            Experience
+          </Button>
         </a>
         <a href="#connectSection" className="menuNavbar">
-          <Button className="nav-btn">Connect</Button>
+          <Button className="nav-btn">
+            <PhoneAndroidIcon className="menuIcon" color="primary" />
+            Connect
+          </Button>
         </a>
         <a className="menuNavbar">
           <Button
             className="nav-btn"
             onClick={() => openInNewTab(LINKS.GITHUB_LINK)}
           >
+            <GitHubIcon className="menuIcon" color="primary" />
             Github
           </Button>
         </a>
@@ -36,6 +60,7 @@ const MenuHeader = ({ width, height }) => {
             className="nav-btn"
             onClick={() => openInNewTab(LINKS.RESUME_LINK)}
           >
+            <DescriptionIcon className="menuIcon" color="primary" />
             Resume
           </Button>
         </a>
